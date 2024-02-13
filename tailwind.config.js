be2f10c5b73a03311892cf8672f4648a90fc3a77
Juan Esteban <juanestban000@gmail.com>
2024-02-13 16:22:20 -0500

    setup packages neccessary

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  colors: {
+    blue: {
+      50: '#EBF3FE',
+      100: '#D8E6FD',
+      200: '#B1CEFB',
+      300: '#8AB5FA',
+      400: '#639CF8',
+      500: '#3B82F6',
+      600: '#0B60EA',
+      700: '#0848B0',
+      800: '#053075',
+      900: '#03183B',
+      950: '#010C1D',
+    },
+  },
+};
